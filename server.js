9389a4716d6223e0746459293374e50639755344
Aryan8451 <107247508+Aryan8451@users.noreply.github.com>
2023-08-25 18:35:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ import productRoutes from "./routes/productRoutes.js"
 import cors from "cors"
 import path from "path"
 import { fileURLToPath } from "url"
-
+ 
 
 //configuring dotenv
 dotenv.config()
